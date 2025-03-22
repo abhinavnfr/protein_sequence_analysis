07b96006e7eaf66bef38968fba9405efa1612c5e
abhinavnfr <abhinavrana18july@gmail.com>
2025-03-22 23:44:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ import streamlit as st
 
 
 def main():
-    st.markdown("<h1 style='color: black;'>Protein Sequence Analysis Workflow Application</h1>", unsafe_allow_html=True)
+    st.markdown("<h1 style='color: black;'>Protein Sequence Analysis App</h1><br>", unsafe_allow_html=True)
+    st.markdown('<p class="right-align">by Abhinav Rana</p>', unsafe_allow_html=True)
 
 if __name__ == "__main__":
     main()
