81c8f56548d1885abea79d0a1b3e1f58dc64c337
abhinavnfr <abhinavrana18july@gmail.com>
2025-03-22 23:36:55 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+import os
+import streamlit as st
+
+
+def main():
+    st.markdown("<h1 style='color: black;'>Protein Sequence Analysis Workflow Application</h1>", unsafe_allow_html=True")
+
+if __name__ == "__main__":
+    main()
