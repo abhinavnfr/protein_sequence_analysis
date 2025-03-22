843f2b3b6e15a9ae95ac65db434dc3be584edc10
abhinavnfr <abhinavrana18july@gmail.com>
2025-03-23 00:29:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,12 +7,16 @@ def main():
     st.markdown("<p style='text-align: right; color: #00BFFF;'>by Abhinav Rana</p><br>", unsafe_allow_html=True)
 
     # file uploader
-    input_file = st.file_uploader(label="To get started, choose a text file containing accession numbers", type=["txt"], accept_multiple_files=False, label_visibility="visible")
+    st.markdown("<p style='color: black;'>To get started, choose a text file containing accession numbers</p><br>", unsafe_allow_html=True)
+    input_file = st.file_uploader(label="Upload file", type=["txt"], accept_multiple_files=False, label_visibility="visible")
 
+    st.markdown("<p></p><br>", unsafe_allow_html=True)
+
+    # fetch FASTA sequences
     st.markdown("<p style='color: black;'>Step 1: Fetch FASTA sequences from accession numbers</p><br>", unsafe_allow_html=True)
     if st.button(label="Click to fetch FASTA sequences", type="primary"):
             st.write("Hello")
-    st.button("Reset", type="secondary")             
+    st.button("Reset", type="secondary")     
                   
 
 if __name__ == "__main__":
