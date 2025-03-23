62c1542429d371eac3239df46ffabbb01497bdef
abhinavnfr <abhinavrana18july@gmail.com>
2025-03-23 19:45:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -32,6 +32,7 @@ def main():
     else:
             num_seq = total_accessions
     num_hits = st.number_input(label="Enter the number of top hits required for each sequence", value=0, step=1)
+    blast_file = False # initialize blast_file
     if st.button(label="Click to BLAST the FASTA sequences", type="primary"):
             blast_file = bs.generate_blast_dataframe(fasta_file, num_seq, num_hits)
     if blast_file:
