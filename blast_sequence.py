ff78a3eecfc2c70aa0b08076a035b2780dab2a15
abhinavnfr <abhinavrana18july@gmail.com>
2025-03-23 20:00:04 +0530

    Update blast_sequence.py

--- blast_sequence.py
+++ blast_sequence.py
@@ -97,7 +97,7 @@ def generate_blast_dataframe(fasta_file, num_seq, num_hits):
     Returns:
         str: The filename of the generated Excel file containing BLAST results.
     """
-    st.write("Starting BLAST")
+    st.write("Starting BLAST.....")
     df = pd.DataFrame(columns=["Complete_Input", "Input_Sequence_ID", "Input_Sequence_Description", "Input_Sequence",
                                "Top_1st_Percent_Identity", "Top_1st_Scientific_Name", "Top_1st_Accession",
                                "Top_2nd_Percent_Identity", "Top_2nd_Scientific_Name", "Top_2nd_Accession",
@@ -107,8 +107,9 @@ def generate_blast_dataframe(fasta_file, num_seq, num_hits):
     for i in range(num_seq):
         lst = blast_sequence(fasta_file, i, num_hits)
         df = pd.concat([df, pd.DataFrame([lst], columns=df.columns)], ignore_index=True)
-
+    
     # save dataframe to an excel file
     excel_file = "blast_results.xlsx"
     df.to_excel(excel_file, index=False)
+    st.write("BLAST finished")
     return excel_file
