2c5df60a159a47c9133177e42f4fe83e1dda79ad
abhinavnfr <abhinavnfr>
2025-09-22 10:40:17 +0000

    update add accession to uc table

--- ingest_sequence.py
+++ ingest_sequence.py
@@ -58,10 +58,10 @@ def add_new_accession_uc_table(accessions: list):
             cursor.close()
             conn.close()
 
-            st.success(f"Successfully added new record into UC table {uc_table} for accession: {accession}")
+            st.success(f"Successfully added new record into UC table {uc_table} for accession: {accessions}")
 
         except Exception as e:
-            st.error(f"Error adding new record for accession {accession} into UC table {uc_table}: {e}")
+            st.error(f"Error adding new record for accession {accessions} into UC table {uc_table}: {e}")
 
 
 # fetch FASTA sequence from accession number
